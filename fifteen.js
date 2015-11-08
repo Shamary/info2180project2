bddf71f9caac4a2af59ca39605e0bf4d510cc98e
shamary <shamarywilliams@gmail.com>
2015-11-08 23:17:59 +0000

    final

--- fifteen.js
+++ fifteen.js
@@ -21,7 +21,7 @@ window.onload = function()
     setup_board(blocks,pos_x,pos_y,empty_tile,sButton);
 }
 
-var getNeighbour = function(blocks,empty_tile) //gets all the neighbours of the empty tile
+var getNeighbour = function(blocks,empty_tile) //get all the neighbours of the empty tile
 {
     var n=new Array();//array of tiles
     var pos=0;
@@ -55,11 +55,11 @@ var swap=function(empty_tile,block)
     
     var timer;
     
-    if(parseInt(block.style.top)!=temp_y)
+    if(parseInt(block.style.top)!=temp_y)/////vertical shift
     {
         timer= setInterval(function(){moveTile(block,true,temp_x,temp_y,timer);},1); 
     }
-    else if(parseInt(block.style.left)!=temp_x)
+    else if(parseInt(block.style.left)!=temp_x)//////horizontal shift
     {
         timer= setInterval(function(){moveTile(block,false,temp_x,temp_y,timer);},1); 
     }
@@ -71,22 +71,22 @@ var moveTile=function(block,vertical,x,y,timer)
 {
     if(vertical)
     {
-        if(parseInt(block.style.top)<y)
+        if(parseInt(block.style.top)<y)////shift down
         {
             block.style.top= (parseInt(block.style.top)+1) +"px" 
         }
-        else if(parseInt(block.style.top)>y)
+        else if(parseInt(block.style.top)>y)//////shift up
         {
             block.style.top= (parseInt(block.style.top)-1) +"px" 
         }
     }
     else
     {
-        if(parseInt(block.style.left)<x)
+        if(parseInt(block.style.left)<x)/////shift left
         {
             block.style.left= (parseInt(block.style.left)+1) +"px" 
         }
-        else if(parseInt(block.style.left)>x)
+        else if(parseInt(block.style.left)>x)///////shift right
         {
             block.style.left= (parseInt(block.style.left)-1) +"px" 
         }
@@ -94,31 +94,31 @@ var moveTile=function(block,vertical,x,y,timer)
     
     if(parseInt(block.style.top)==y&&parseInt(block.style.left)==x)
     {
-        clearInterval(timer);
+        clearInterval(timer);///stop timer
     }
 }
 
 
-var set_correct= function(blocks)
+var set_correct= function(blocks)/////////correct image layout
 {
-    blocks[0].style.backgroundPosition="0px 50PX";
-    blocks[1].style.backgroundPosition="-100px 50PX";
-    blocks[2].style.backgroundPosition="-225px 50PX";
-    blocks[3].style.backgroundPosition="-321px 50PX";
+    blocks[0].style.backgroundPosition="10px 50PX";
+    blocks[1].style.backgroundPosition="-90px 50PX";
+    blocks[2].style.backgroundPosition="-214px 50PX";
+    blocks[3].style.backgroundPosition="-311px 51PX";
     ////////row 2
-    blocks[4].style.backgroundPosition="0px -49PX";
-    blocks[5].style.backgroundPosition="-101px -49PX";
-    blocks[6].style.backgroundPosition="-225px -50PX";
-    blocks[7].style.backgroundPosition="-321px -49PX";
+    blocks[4].style.backgroundPosition="10px -49PX";
+    blocks[5].style.backgroundPosition="-91px -49PX";
+    blocks[6].style.backgroundPosition="-215px -50PX";
+    blocks[7].style.backgroundPosition="-311px -49PX";
     ////////row 3
-    blocks[8].style.backgroundPosition="0px -145PX";
-    blocks[9].style.backgroundPosition="-102px -147PX";
-    blocks[10].style.backgroundPosition="-227px -145PX";
-    blocks[11].style.backgroundPosition="-321px -147PX";
+    blocks[8].style.backgroundPosition="10px -145PX";
+    blocks[9].style.backgroundPosition="-92px -147PX";
+    blocks[10].style.backgroundPosition="-217px -145PX";
+    blocks[11].style.backgroundPosition="-311px -147PX";
     ///row 4
-    blocks[12].style.backgroundPosition="0px -244PX";
-    blocks[13].style.backgroundPosition="-106px -244PX";
-    blocks[14].style.backgroundPosition="-227px -244PX";
+    blocks[12].style.backgroundPosition="10px -244PX";
+    blocks[13].style.backgroundPosition="-96px -244PX";
+    blocks[14].style.backgroundPosition="-217px -244PX";
 }
 
 
@@ -129,7 +129,7 @@ var setup_board= function(blocks,pos_x,pos_y,empty_tile,sButton)////Initial boar
        
         blocks[i].addClassName("puzzlepiece");
         
-        blocks[i].style.backgroundRepeat="no-repeat";
+        blocks[i].style.backgroundRepeat="no-repeat";//////no repeat images in background
         
         ///////////position the tiles////////////
         blocks[i].style.left=(pos_x+"px");
@@ -181,13 +181,13 @@ var setup_board= function(blocks,pos_x,pos_y,empty_tile,sButton)////Initial boar
         
         blocks[i].onmouseout = function()
         {
-            this.className="puzzlepiece";
+            this.className="puzzlepiece";/////reset tile to original state
         };
     }
     
     sButton.onmousedown=function() //////shuffle button
         {
-            var nlist=new Array();
+            var nlist=new Array();///////neighbour array
             var r=0,temp_x=0,temp_y=0;
             
             for(var p=0;p<300;p++)
